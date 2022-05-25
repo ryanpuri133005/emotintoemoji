2fe2caf9f453babfc823a6901bdc9a79b6eb11bd
ryanpuri133005 <95753861+ryanpuri133005@users.noreply.github.com>
2022-05-25 18:03:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+Webcam.set({
+  width: 350,
+  height: 300,
+  image_format: "png",
+  png_quality: 90,
+});
+Camera = document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot() {
+  Webcam.snap(function (data_uri) {
+    document.getElementById("result").innerHTML =
+      '<img id="captured_image" src="' + data_uri + '">';
+  });
+}
+console.log("ml5 version:", ml5.version);
+classifier = ml5.imageClassifier(
+  "https://teachablemachine.withgoogle.com/models/-d1sV02NY/model.json",
+  modelLoaded
+);
+function modelLoaded() {
+  console.log("model loaded successfuly");
+}
+
+function speak() {
+  var synth = window.speechSynthesis;
+  speak_data_1 = "the first prediction is " + prediction_1;
+  speak_data_2 = "and the second prediction is " + prediction_2;
+  var utterthis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterthis);
+}
+function check() {
+  img = document.getElementById("captured_image");
+  classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML =
+      results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if (results[0].label=="happy") {
+      document.getElementById("update_emoji").innerHTML = "&#128522;";  
+      
+    }
+     if (results[0].label == "sad") {
+       document.getElementById("update_emoji").innerHTML = "&#128532;";
+     }
+     if (results[0].label == "Fortnite rage quitter") {
+       document.getElementById("update_emoji").innerHTML = "&#128548;";
+     }
+     if (results[0].label == "shocked") {
+       document.getElementById("update_emoji").innerHTML = "&#128562;";
+     }
+    
+     if (results[1].label == "happy") {
+       document.getElementById("update_emoji2").innerHTML = "&#128522;";
+     }
+     if (results[1].label == "sad") {
+       document.getElementById("update_emoji2").innerHTML = "&#128532;";
+     }
+     if (results[1].label == "Fortnite rage quitter") {
+       document.getElementById("update_emoji2").innerHTML = "&#128548;";
+     }
+      if (results[1].label == "shocked") {
+        document.getElementById("update_emoji2").innerHTML = "&#128562;";
+      }
+  }
+}
